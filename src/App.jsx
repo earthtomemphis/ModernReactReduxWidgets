d3bf0d9b47a43c0d3143619febe77b87aaaeeb6b
Memphis DiAngelis <earthtomemphis@gmail.com>
2022-03-31 03:40:53 -0600

    Dropdown widget

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 
 const items = [
@@ -17,10 +18,37 @@ const items = [
 	},
 ];
 
+const options = [
+	{
+		label: 'The Color Red',
+		value: 'red',
+	},
+	{
+		label: 'The Color Green',
+		value: 'green',
+	},
+	{
+		label: 'A Shade of Blue',
+		value: 'blue',
+	},
+];
+
 export default () => {
+	const [selected, setSelected] = useState(options[0]);
+	const [showDropdown, setShowDropdown] = useState(true);
+
 	return (
 		<div>
-			<Search />
+			<button onClick={() => setShowDropdown(!showDropdown)}>
+				Toggle Dropdown
+			</button>
+			{showDropdown ? (
+				<Dropdown
+					selected={selected}
+					onSelectedChange={setSelected}
+					options={options}
+				/>
+			) : null}
 		</div>
 	);
 };
